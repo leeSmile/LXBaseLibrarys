94c263a1e150f961653cc4d27ceeef407e6a2e5f
李枭 <244645877@qq.com>
2018-11-23 11:22:10 +0800

    提交代码

---
+++ DYColorConst.h
@@ -0,0 +1,26 @@
+//
+//  DYColorConst.h
+//  QEZB
+//
+//  Created by 李超群 on 2018/11/7.
+//  Copyright © 2018 zhou. All rights reserved.
+//
+
+#define kColor_gray_000000 [UIColor colorFromHexRGB:@"000000"]
+#define kColor_gray_333333 [UIColor colorFromHexRGB:@"333333"]
+#define kColor_gray_4A4A4A [UIColor colorFromHexRGB:@"4A4A4A"]
+#define kColor_gray_999999 [UIColor colorFromHexRGB:@"999999"]
+#define kColor_gray_9B9B9B [UIColor colorFromHexRGB:@"9B9B9B"]
+#define kColor_gray_D8D8D8 [UIColor colorFromHexRGB:@"D8D8D8"]
+#define kColor_gray_E2E2E2 [UIColor colorFromHexRGB:@"E2E2E2"]
+#define kColor_gray_EAEAEA [UIColor colorFromHexRGB:@"EAEAEA"]
+#define kColor_gray_EFEFEF [UIColor colorFromHexRGB:@"EFEFEF"]
+#define kColor_gray_F5F5F5 [UIColor colorFromHexRGB:@"F5F5F5"]
+#define kColor_gray_FFFFFF [UIColor colorFromHexRGB:@"FFFFFF"]
+
+
+#define kColor_yellow_FFD800 [UIColor colorFromHexRGB:@"FFD800"]
+#define kColor_yellow_FFB500 [UIColor colorFromHexRGB:@"FFB500"]
+
+#define kcolor_red_F74C31 [UIColor colorFromHexRGB:@"F74C31"]
+#define kcolor_red_FF5655 [UIColor colorFromHexRGB:@"FF5655"]

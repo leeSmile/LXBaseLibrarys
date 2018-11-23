d322aa3ca8518472bac51ef503b2d3e1dffe6bae
李枭 <244645877@qq.com>
2018-11-23 13:45:10 +0800

    1.0.1

---
+++ LXFontConst.h
@@ -0,0 +1,12 @@
+
+//
+//  DYFontConst.h
+//  QEZB
+//
+//  Created by 李超群 on 2018/11/8.
+//  Copyright © 2018 zhou. All rights reserved.
+//
+
+
+#define kFont_System_bold(size) [UIFont systemFontOfSize:(size) weight:UIFontWeightSemibold]
+#define kFont_System_normal(size) [UIFont systemFontOfSize:(size)]
